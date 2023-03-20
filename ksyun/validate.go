847714fc9b548c3ff8d7c60edf59a64fe07222c9
cqc <cqclife@qq.com>
2023-03-20 17:27:11 +0800

    no message

--- ksyun/validate.go
+++ ksyun/validate.go
@@ -5,12 +5,14 @@ import (
 	"time"
 )
 
+const Iso8601DateFormat = "2006-01-02T00:00:00+08:00"
+
 func validateKs3BucketDateTimestamp(v interface{}, k string) (ws []string, errors []error) {
 	value := v.(string)
-	_, err := time.Parse("yyyy-MM-dd", value)
+	_, err := time.Parse(Iso8601DateFormat, value)
 	if err != nil {
 		errors = append(errors, fmt.Errorf(
-			"%q cannot be parsed as date YYYY-MM-DD Format", value))
+			"%q cannot be parsed as date %s Format", value, Iso8601DateFormat))
 	}
 	return
 }
